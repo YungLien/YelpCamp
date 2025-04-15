aed8262e5cf40ce8b58544a83c64defafd5e10ca
YungLien <wuyunglien@gmail.com>
2025-04-15 22:59:36 +1000

    change the port

--- app.js
+++ app.js
@@ -184,7 +184,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err }); // 將 err 傳遞給模板
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
 
